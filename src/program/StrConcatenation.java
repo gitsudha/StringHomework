48a00b88e4dee96ea6d33872a8aa28f67c812a81
gitsudha <sudha.upperi@gmail.com>
2019-01-11 19:32:05 +0000

    first commit

---
+++ src/program/StrConcatenation.java
@@ -0,0 +1,14 @@
+package program;
+
+import java.sql.SQLOutput;
+
+public class StrConcatenation {
+    public static void main(String[] args) {
+        String str =" ";
+        for(int i =20;i>=1;i--)
+        {
+            str = str+i;
+        }
+        System.out.println( str );
+    }
+}
